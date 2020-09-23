5bf31576e12302568b3e0eeaa5b6c75aced4ec69
Mroxny <maciej.h.mrozek@gmail.com>
2020-09-23 22:40:02 +0200

    AddProject

---
+++ Assets/Maciek/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
