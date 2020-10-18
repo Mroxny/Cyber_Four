6a2a608d69c380a5e2f3d632a7d562f6eb817a2e
Mroxny <maciej.h.mrozek@gmail.com>
2020-10-18 18:38:03 +0200

    Casual Update

--- Assets/Maciek/Scripts/Player.cs
+++ Assets/Maciek/Scripts/Player.cs
@@ -5,30 +5,41 @@ using Mirror;
 
 public class Player : MonoBehaviour {
 
-    private Animator animator;
-
+    public GameObject cam;
     
-    void Start() {
-        SetSkin();
-    }
-    void SetSkin() {
+    private Animator animator;
+    GameObject weaponRender;
 
+    void Start() {
+        if (cam == null) {
+            cam = GameObject.Find("Main Camera");
+        }
+        
+        SetChar();
+
+    }
+    void SetChar() {
+        
         switch (PlayerPrefs.GetInt("CharacterId")) {
             case 1:
                 transform.GetChild(1 - 1).gameObject.SetActive(true);
                 animator = transform.GetChild(1 - 1).transform.GetChild(0).GetComponent<Animator>();
+                weaponRender = transform.GetChild(1 - 1).transform.GetChild(0).GetComponent<GameObject>();
                 break;
             case 2:
                 transform.GetChild(2 - 1).gameObject.SetActive(true);
                 animator = transform.GetChild(2 - 1).transform.GetChild(0).GetComponent<Animator>();
+                weaponRender = transform.GetChild(2 - 1).transform.GetChild(0).GetComponent<GameObject>();
                 break;
             case 3:
                 transform.GetChild(3 - 1).gameObject.SetActive(true);
                 animator = transform.GetChild(3 - 1).transform.GetChild(0).GetComponent<Animator>();
+                weaponRender = transform.GetChild(3 - 1).transform.GetChild(0).GetComponent<GameObject>();
                 break;
             case 4:
                 transform.GetChild(4 - 1).gameObject.SetActive(true);
                 animator = transform.GetChild(4 - 1).transform.GetChild(0).GetComponent<Animator>();
+                weaponRender = transform.GetChild(4 - 1).transform.GetChild(0).GetComponent<GameObject>();
                 break;
         }
 
@@ -41,9 +52,10 @@ public class Player : MonoBehaviour {
         transform.position = new Vector2(transform.position.x + (x*0.1f), transform.position.y + (y * 0.1f));
 
         Vector2 movement = new Vector2(x,y);
-        print(x);
         animator.SetFloat("Horizontal", x);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(transform.position.x, transform.position.y,-10f), Time.deltaTime*5f);
+        weaponRender.transform.Rotate(1f, 0.0f, 0.0f, Space.Self);
 
     }
     
